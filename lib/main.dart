52064f6d7f62a0a87b85c426aec723970aac4e39
Saurav Sunar <sunarsaurav01@gmail.com>
2022-10-12 22:11:58 +0545

    working with card class

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class myApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
@@ -36,6 +37,51 @@ class myApp extends StatelessWidget {
                   fontFamily: 'Source Sans Pro',
                   fontSize: 20.0,
                   fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 200,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              //padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+977 9826160015',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              //padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'sunarsaurav6@gmail.com',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0),
+                ),
+              ),
             )
           ],
         )),
